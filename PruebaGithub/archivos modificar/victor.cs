9624c92f35c2590265b82d57ded5bc5bf2911b56
Victor Manuel Fabregas Castro (Helpdesk) <helpdesk@eposbcn.es>
2020-11-11 16:15:37 +0100

    Agregar archivos de proyecto.

---
+++ PruebaGithub/archivos modificar/victor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaGithub.archivos_modificar
+{
+    class victor
+    {
+        /*
+         skdfmjkdsnjfkmcds
+        skdfmfkmsdkfmsdkfmkds
+
+        skdfmksdmfkdsmfs
+
+        s,lmfksdfl,*/
+    }
+}
